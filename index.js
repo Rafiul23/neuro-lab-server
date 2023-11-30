08d1a2c7f92f2e7df7a8fbe609147e41fbc1edfd
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-30 09:09:43 +0600

    banner can be deleted by delete operation

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/banner/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bannerCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/banners', async (req, res) => {
       const result = await bannerCollection.find().toArray();
       res.send(result);
