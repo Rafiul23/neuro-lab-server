d749b7bad87e59ddb0dea32c367686932a246616
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-30 13:23:06 +0600

    added verification of jwt token and admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 })
+
+ // middlewares
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlof2pa.mongodb.net/?retryWrites=true&w=majority`;
@@ -46,21 +51,74 @@ async function run() {
     const appointmentCollection = client.db('labDB').collection('appointments');
 
 
+   
+
+    // jwt related api
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'});
+      res.send({token});
+    })
+
+    const verifyToken = (req, res, next)=>{
+      console.log('inside verify token', req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'Unauthorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      console.log(token)
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+        if(error){
+          return res.status(401).send({message:'Unauthorized access'});
+        } 
+        req.decoded = decoded;
+        next();
+      })
+      
+    }
+
+    app.get('/user/admin/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return req.status(403).send({message: 'Forbidden Access'});
+      }
+      const query = { email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
+    })
+
+    // middleware
+    const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = { email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message:'Forbidden Access'})
+      } else {
+        next();
+      }
+    }
+
     // appointments related api
-    app.post('/appointment', async (req, res) => {
+    app.post('/appointment', verifyToken, async (req, res) => {
       const appointment = req.body;
       const result = await appointmentCollection.insertOne(appointment);
       res.send(result);
     })
 
-    app.get('/appointments/:email', async (req, res) => {
+    app.get('/appointments/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await appointmentCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.delete('/appointment/:id', async(req, res)=>{
+    app.delete('/appointment/:id', verifyToken, async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await appointmentCollection.deleteOne(query);
@@ -69,20 +127,20 @@ async function run() {
 
 
     // banner related api
-    app.post('/banners', async (req, res) => {
+    app.post('/banners', verifyToken, verifyAdmin, async (req, res) => {
       const bannerInfo = req.body;
       const result = await bannerCollection.insertOne(bannerInfo);
       res.send(result);
     })
 
-    app.delete('/banner/:id', async(req, res)=>{
+    app.delete('/banner/:id', verifyToken, verifyAdmin, async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await bannerCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.get('/banners', async (req, res) => {
+    app.get('/banners', verifyToken, verifyAdmin, async (req, res) => {
       const result = await bannerCollection.find().toArray();
       res.send(result);
     })
@@ -101,7 +159,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/test/:id', async (req, res) => {
+    app.delete('/test/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await allTestCollection.deleteOne(query);
@@ -115,13 +173,13 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/test', async (req, res) => {
+    app.post('/test', verifyToken, verifyAdmin, async (req, res) => {
       const test = req.body;
       const result = await allTestCollection.insertOne(test);
       res.send(result);
     })
 
-    app.put('/test/:id', async (req, res) => {
+    app.put('/test/:id', verifyToken, verifyAdmin, async (req, res) => {
       const test = req.body;
       console.log(test)
       const id = req.params.id;
@@ -141,8 +199,11 @@ async function run() {
 
     })
 
+   
 
     // user related api
+
+   
 
     app.post('/users', async (req, res) => {
 
@@ -156,14 +217,14 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
     // make user admin
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -176,7 +237,7 @@ async function run() {
     })
 
     // block a user
-    app.put('/users/block/:id', async (req, res) => {
+    app.put('/users/block/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedStatus = {
@@ -189,7 +250,7 @@ async function run() {
     })
 
     // active a user
-    app.put('/users/active/:id', async (req, res) => {
+    app.put('/users/active/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -202,7 +263,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users/:email', async (req, res) => {
+    app.get('/users/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await userCollection.findOne(query);
@@ -210,7 +271,7 @@ async function run() {
     })
 
     // payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyToken, async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
       console.log(amount);
